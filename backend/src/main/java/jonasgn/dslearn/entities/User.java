0d74f3e8d43a7e0c87801ce7bcb31d73c177997a
jonasGN <j.goncalvesneto@outlook.com>
2021-10-29 15:51:57 -0300

    Notification add

--- backend/src/main/java/jonasgn/dslearn/entities/User.java
+++ backend/src/main/java/jonasgn/dslearn/entities/User.java
@@ -1,7 +1,9 @@
 package jonasgn.dslearn.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
@@ -14,6 +16,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -33,6 +36,9 @@ public class User implements Serializable {
 	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
+
+	@OneToMany(mappedBy = "user")
+	private List<Notification> notifications = new ArrayList<>();
 
 	public User() {
 	}
@@ -78,6 +84,10 @@ public class User implements Serializable {
 
 	public Set<Role> getRoles() {
 		return roles;
+	}
+
+	public List<Notification> getNotifications() {
+		return notifications;
 	}
 
 	@Override
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -44,3 +44,7 @@ INSERT INTO tb_lesson (title, position, section_id) VALUES ('Tarefa 01 - Capitul
 INSERT INTO tb_task (id, description, question_count, approval_count, weight, due_date) VALUES (3, 'Tarefa de aprovação 01', 5, 4, 1.0, TIMESTAMP WITH TIME ZONE '2020-07-30T20:50:07Z');
 
 INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (1, 2, 2);
+
+INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Primeiro feedback de tarefa: favor revisar', TIMESTAMP WITH TIME ZONE '2020-12-10T13:00:00Z', true, '/offers/1/resource/1/sections/1', 2);
+INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Segundo feedback: favor revisar', TIMESTAMP WITH TIME ZONE '2020-12-12T13:00:00Z', true, '/offers/1/resource/1/sections/1', 2);
+INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Terceiro feedback: favor revisar', TIMESTAMP WITH TIME ZONE '2020-12-14T13:00:00Z', true, '/offers/1/resource/1/sections/1', 2);
